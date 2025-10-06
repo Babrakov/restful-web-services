4498d60c56af6fcff3801136d8dad16202f42992
Sergei Babrakov <s.a.babrakov@gmail.com>
2025-10-06 08:55:57 +0200

    Internationalization

--- src/main/java/ru/infoza/restfulwebservices/controller/HelloWorldController.java
+++ src/main/java/ru/infoza/restfulwebservices/controller/HelloWorldController.java
@@ -1,9 +1,15 @@
 package ru.infoza.restfulwebservices.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequiredArgsConstructor
 public class HelloWorldController {
+
+    private final MessageSource messageSource;
 
     @GetMapping( "/hello")
     public String sayHello() {
@@ -19,5 +25,12 @@ public class HelloWorldController {
     public HelloWorldBean sayHelloBeanPathVariable(@PathVariable String name) {
         return new HelloWorldBean(String.format("Hello World, %s!", name));
     }
+
+    @GetMapping( "/hello-i18n")
+    public String sayHelloInternationalized() {
+        var locale = LocaleContextHolder.getLocale();
+        return messageSource.getMessage("good.morning.message",null, "Default Message", locale);
+    }
+
 
 }
