f3a13557e133d0c161f167c0cb3de43176671b12
Sergei Babrakov <s.a.babrakov@gmail.com>
2025-10-09 08:55:11 +0200

    Request params, headers and mediatype for versions

--- src/main/java/ru/infoza/restfulwebservices/versioning/PersonV2.java
+++ src/main/java/ru/infoza/restfulwebservices/versioning/PersonV2.java
@@ -1,8 +1,10 @@
 package ru.infoza.restfulwebservices.versioning;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 
 @AllArgsConstructor
+@Getter
 public class PersonV2 {
 
     private String firstName;
--- src/main/java/ru/infoza/restfulwebservices/versioning/VersioningPersonController.java
+++ src/main/java/ru/infoza/restfulwebservices/versioning/VersioningPersonController.java
@@ -7,12 +7,42 @@ import org.springframework.web.bind.annotation.RestController;
 public class VersioningPersonController {
 
     @GetMapping("/v1/person")
-    public PersonV1 gerFirstVersionOfPerson() {
+    public PersonV1 getFirstVersionOfPerson() {
         return new PersonV1("John Doe");
     }
 
     @GetMapping("/v2/person")
-    public PersonV2 gerSecondVersionOfPerson() {
+    public PersonV2 getSecondVersionOfPerson() {
+        return new PersonV2("John", "Doe");
+    }
+
+    @GetMapping(path = "/person", params = "version=1")
+    public PersonV1 getFirstVersionOfPersonParam() {
+        return new PersonV1("John Doe");
+    }
+
+    @GetMapping(path = "/person", params = "version=2")
+    public PersonV2 getSecondVersionOfPersonParam() {
+        return new PersonV2("John", "Doe");
+    }
+
+    @GetMapping(path = "/person-header", headers = "X-API-VERSION=1")
+    public PersonV1 getFirstVersionOfPersonHeader() {
+        return new PersonV1("John Doe");
+    }
+
+    @GetMapping(path = "/person-header", headers = "X-API-VERSION=2")
+    public PersonV2 getSecondVersionOfPersonHeader() {
+        return new PersonV2("John", "Doe");
+    }
+
+    @GetMapping(path = "/person-accept", produces = "application/v1+json")
+    public PersonV1 getFirstVersionOfPersonAccept() {
+        return new PersonV1("John Doe");
+    }
+
+    @GetMapping(path = "/person-accept", produces = "application/v2+json")
+    public PersonV2 getSecondVersionOfPersonAccept() {
         return new PersonV2("John", "Doe");
     }
 
